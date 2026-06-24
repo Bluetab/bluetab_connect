19d51d84f6bbedc89cc850081861e5cb685edcae
netto <euclydes.young@bluetab.net>
2026-06-24 17:02:43 +0200

    chore: update project version to 26.6.24

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BluetabConnect.MixProject do
   def project do
     [
       app: :bluetab_connect,
-      version: "26.6.18",
+      version: "26.6.24",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps()
