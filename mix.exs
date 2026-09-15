8a1f7e61e8a98be7e6c81b7f44f85d66c9eb95bc
netto <euclydes.young@bluetab.net>
2026-09-15 12:31:15 +0200

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BluetabConnect.MixProject do
   def project do
     [
       app: :bluetab_connect,
-      version: "26.9.11",
+      version: "26.9.15",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps()
